d3fb49cfe553db38488285fc38e77cdca518a8d2
JonathanPoitrenaud <poitrenaudjonathan@gmail.com>
2021-11-30 17:16:44 +0100

    Step 5 - advance routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", "name=program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Wide Vtech',
+        ]);
+    }
+
+    /**
+     * @Route("/list/{page}", requirements={"page"="\d+"}, name="list")
+     */
+    public function list(int $page = 4): Response
+    {
+        return $this->render('program/list.html.twig', [
+           'page' => $page,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(): Response
+    {
+        return $this->render('program/new.html.twig');
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="id")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
         ]);
     }
 }
